1ef1dee8604d162c722a3b5f94e2a70750933ed0
Mircea Negrău <nmircea17@gmail.com>
2020-02-14 22:23:22 -0800

    Restructuring

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -14,6 +14,7 @@ namespace Catalog
         [STAThread]
         static void Main()
         {
+            //test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Login_Form fLogin = new Login_Form();
